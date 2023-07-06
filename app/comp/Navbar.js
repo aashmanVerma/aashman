48158cf361e17156769f108a33b0936193b02f8a
Elite16 <vermaaashman16@gmail.com>
2023-07-06 11:52:42 +0530

    nav fix

--- app/comp/Navbar.js
+++ app/comp/Navbar.js
@@ -35,7 +35,9 @@ export default function Navbar() {
                 }}>Contact</span>
             </div>
             <div className="lg:hidden" onClick={()=>setToggle(!toggle)}>
-                <svg width="25px" height="25px" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M3 5h18M3 12h18M3 19h18" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
+                {!toggle ? <svg width="25px" height="25px" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M3 5h18M3 12h18M3 19h18" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg> :
+                <svg width="25px" height="25px" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M6.758 17.243L12.001 12m5.243-5.243L12 12m0 0L6.758 6.757M12.001 12l5.243 5.243" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
+                }
             </div>
         </div>
         <AnimatePresence>
